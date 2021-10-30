a65f34a8bb477438e534b44f73961f6ce5ea66cc
“Houssine” <person.zen15@gmail.com>
2021-10-30 11:32:58 +0100

    add basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/rest-countries-api">
       <App />
     </HashRouter>
   </React.StrictMode>,
